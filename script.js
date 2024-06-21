3a32fbf742733e4c48b800e2be8211050e2b63c7
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-06-21 20:23:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
+// Player 1
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 
-if (randomNumber1 === 1){
-    document.querySelector("img.img1").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber1 === 2){
-    document.querySelector("img.img1").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber1 === 3){
-    document.querySelector("img.img1").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber1 === 4){
-    document.querySelector("img.img1").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber1 === 5){
-    document.querySelector("img.img1").setAttribute("src", "./images/dice5.png");
-} else if (randomNumber1 === 6){
-    document.querySelector("img.img1").setAttribute("src", "./images/dice6.png");
-}
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
+
+
+// Player 2
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
-if (randomNumber2 === 1){
-    document.querySelector("img.img2").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber2 === 2){
-    document.querySelector("img.img2").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber2 === 3){
-    document.querySelector("img.img2").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber2 === 4){
-    document.querySelector("img.img2").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber2 === 5){
-    document.querySelector("img.img2").setAttribute("src", "./images/dice5.png");
-} else if (randomNumber2 === 6){
-    document.querySelector("img.img2").setAttribute("src", "./images/dice6.png");
-}
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+
+// Title change
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1){
